e6a2a0c750cddc7c7bdf3e6690447942562ae387
Nikhilesh-H <97732838+Nikhilesh-H@users.noreply.github.com>
2024-09-05 20:16:25 +0530

    Update GDP-Map.py

--- GDP-Map.py
+++ GDP-Map.py
@@ -5,7 +5,7 @@ import io
 import os
 from PIL import Image
 
-def extract_zip(file):
+def extract_zip(file, output_dir):
     """Extract the ZIP file and return a dictionary of file names and their content."""
     extracted_files = {}
     with zipfile.ZipFile(file, 'r') as zip_ref:
@@ -13,6 +13,8 @@ def extract_zip(file):
             # Read the file into memory
             with zip_ref.open(file_info.filename) as f:
                 extracted_files[file_info.filename] = f.read()
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
     return extracted_files
 
 def list_files_in_directory(files, directory):
@@ -33,7 +35,7 @@ uploaded_file = st.file_uploader("Choose a ZIP file", type="zip")
 
 if uploaded_file:
     # Extract the ZIP file
-    extracted_files = extract_zip(uploaded_file)
+    extracted_files = extract_zip(uploaded_file, os.getcwd())
     
     # Check for .gdb folder
     gdb_files = [file for file in extracted_files.keys() if ".gdb" in file]
@@ -43,8 +45,7 @@ if uploaded_file:
         st.write("Found .gdb folder. Checking layers...")
         # Assuming the first .gdb file is what you want to use
         gdb_file = gdb_files[0][:-1]  # Adjust if necessary
-        village = village_info(r"C:\Users\heeya\Downloads\Chhattisgarh_2\\" + gdb_file, "selva")
-        
+        village = village_info(gdb_file, *gdb_file.split('/')[-1].split('_')[1].split(".")[:-1])
         layers = village.layers_available()
         
         if layers:
@@ -74,7 +75,11 @@ if uploaded_file:
                         img_data = io.BytesIO(file.read())
                         img = Image.open(img_data)
                         st.image(img, caption="Updated Map Image")
-                else: st.write("Image file not found.")
-            else: st.write("No layers selected. Please select at least one layer.")
-        else: st.write("No layers available in the .gdb folder.")
-    else: st.write("No .gdb folder found in the ZIP archive.")
+                else:
+                    st.write("Image file not found.")
+            else:
+                st.write("No layers selected. Please select at least one layer.")
+        else:
+            st.write("No layers available in the .gdb folder.")
+    else:
+        st.write("No .gdb folder found in the ZIP archive.")
