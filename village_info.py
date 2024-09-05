5ed0002751ec48a0fc7b568416eb6257c8228ca5
Heeya13 <heeyachawda12@gmail.com>
2024-09-05 20:19:45 +0530

    Update village_info.py
    Adding pic_gen_multiple
    Changing gbd to gpd in house_info and road_info
    removing brackets after area

--- village_info.py
+++ village_info.py
@@ -10,15 +10,15 @@ class village_info:
         self.vname = vname
     
     def house_info(self,lname):
-        self.house = gpd.read_file(self.gdb_path,layer=lname)
-        self.house['area'] = self.house['geometry'].area()
+        self.house = gpd.read_file(self.gdp_path,layer=lname)
+        self.house['area'] = self.house['geometry'].area
         self.area = self.house['area'].sum()
         self.count = len(self.house)
         self.avg = self.area/self.count
         return self.count , self.area , self.avg
     
     def road_info(self,lname):
-        self.road = gpd.read_file(self.gdb_path,layer=lname)
+        self.road = gpd.read_file(self.gdp_path,layer=lname)
         self.road['length'] = self.road['geometry'].length
         return self.road['length'].sum()/1000
 
@@ -30,3 +30,17 @@ class village_info:
         self.pic_layer.plot()
         plt.savefig(f'{self.vname}_{lname}.jpeg')
         return f'{self.vname}_{lname}.jpeg'
+
+    def pic_gen_multiple(self, layer_names):
+        fig, ax = plt.subplots(figsize=(10, 10)) 
+        
+        for lname in layer_names:
+            layer = gpd.read_file(self.gdp_path, layer=lname)
+            layer.plot(ax=ax, label=lname)
+        
+        ax.legend()
+        combined_filename = f'{self.vname}_combined_layers.jpeg'
+        plt.savefig(combined_filename)
+        plt.close() 
+        
+        return combined_filename
